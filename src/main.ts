9fa1a2a450659c2e21da7f958167ac44cb678d0c
Victor Saravia <victormsaraviat@gmail.com>
2022-10-16 11:50:45 -0500

    fix: prettier

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v2/api')
+  app.setGlobalPrefix('v2/api');
   await app.listen(3000);
 }
 bootstrap();
