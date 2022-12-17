58e057195fc90b25eff16e163be824b50529fb4f
Victor Saravia <victormsaraviat@gmail.com>
2022-12-17 01:22:21 -0500

    feat: finish pokemon endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonsModule } from './commons/commons.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
     }),
     PokemonModule,
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    CommonsModule,
   ],
 })
 export class AppModule {}
