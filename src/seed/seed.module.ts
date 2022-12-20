81a3d23f20c453eb66f8e1a1decce951eaa819e7
Victor Saravia <victormsaraviat@gmail.com>
2022-12-20 16:51:58 -0500

    fix: update path references

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { PokemonModule } from '../../src/pokemon/pokemon.module';
-import { CommonsModule } from '../../src/commons/commons.module';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { CommonsModule } from 'src/commons/commons.module';
 
 @Module({
   imports: [PokemonModule, CommonsModule],
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,7 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { PokeResponse } from './interfaces/poke-response';
 import { Pokemon, PokemonDocument } from '../pokemon/entities/pokemon.entity';
-import { AxiosAdapter } from '../../src/commons/adapters/axios.adapter';
+import { AxiosAdapter } from 'src/commons/adapters/axios.adapter';
+
 @Injectable()
 export class SeedService {
   constructor(
